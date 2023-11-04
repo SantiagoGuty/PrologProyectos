8b11f176131521f17863c4d6468648b28540942a
SantiagoGuty <128735165+SantiagoGuty@users.noreply.github.com>
2023-11-03 22:11:37 -0500

    Update valpo_swim.pl
    Ordered everything by groups 
    
    and fixed an error in the functions

--- valpo_swim.pl
+++ valpo_swim.pl
@@ -1,20 +1,18 @@
-swimteam.
+% Swimteam declaration
+swimteam :- true.
+
+% Freshmen
 freshman(nate_boolinger).
-sophomore(owen_cullinan).
-junior(santiago_gutierrez_morales).
-junior(andrew_berzai).
-sophomore(ricky_helmbolt).
-junior(alex_koetter).
-senior(ian_jaegers).
 freshman(ian_muffet).
-sophomore(garrett_hoppman).
-junior(brendan_stangeland).
-senior(logan_lorr).
-sophomore(kole_kennedy).
 freshman(sean_toolis).
-sophomore(eli_liebmann).
-junior(ethan_welker).
 freshman(zach_roberts).
+
+% Sophomores
+sophomore(owen_cullinan).
+sophomore(ricky_helmbolt).
+sophomore(garrett_hoppman).
+sophomore(kole_kennedy).
+sophomore(eli_liebmann).
 sophomore(tim_mai).
 sophomore(matthew_mantz).
 sophomore(antony_martin).
@@ -23,40 +21,66 @@ sophomore(tyler_sessler).
 sophomore(luke_snider).
 sophomore(jeremy_suarez).
 sophomore(dathan_wolf).
+
+% Juniors
+junior(santiago_gutierrez_morales).
+junior(andrew_berzai).
+junior(alex_koetter).
+junior(brendan_stangeland).
+junior(ethan_welker).
+
+% Seniors
+senior(ian_jaegers).
+senior(logan_lorr).
 senior(drew_wilson).
-free(nate_boolinger).
-back(owen_cullinan).
-back(santiago_gutierrez_morales).
-medley(andrew_berzai).
-distance(ricky_helmbolt).
-distance(alex_koetter).
-back(ian_jaegers).
-breast(ian_muffet).
+
+% Butterfly Events
+fly(nate_boolinger).
 fly(garrett_hoppman).
-breast(brendan_stangeland).
-free(logan_lorr).
-distance(kole_kennedy).
-free(sean_toolis).
-distance(eli_liebmann).
-medley(ethan_welker).
-distance(zach_roberts).
 fly(tim_mai).
 fly(matthew_mantz).
 fly(antony_martin).
-medley(jackson_oostman).
+fly(jackson_oostman).
 fly(tyler_sessler).
 fly(luke_snider).
-free(jeremy_suarez).
-back(dathan_wolf).
 fly(drew_wilson).
+
+% Backstroke Events
+back(owen_cullinan).
+back(santiago_gutierrez_morales).
+back(ian_jaegers).
 back(drew_wilson).
-breast(drew_wilson).
-free(drew_wilson).
+back(dathan_wolf).
+
+% Medley Events
+medley(andrew_berzai).
+medley(ethan_welker).
+medley(jackson_oostman).
 medley(drew_wilson).
+
+% Distance Events
+distance(ricky_helmbolt).
+distance(alex_koetter).
+distance(kole_kennedy).
+distance(sean_toolis).
+distance(eli_liebmann).
+distance(zach_roberts).
 distance(drew_wilson).
 
-same_stroke(X,Y):- fly(X),fly(Y); back(X),back(Y);breast(X),breast(Y);free(X),free(Y);medley(X),medley(Y); distance(X),distance(Y).
+% Breaststroke Events
+breast(ian_muffet).
+breast(brendan_stangeland).
+breast(drew_wilson).
 
-same_year(X,Y):- freshman(X), freshman(Y); sophomore(X), sophomore(Y); junior(X),junior(Y); senior(X), senior(Y).
+% Freestyle Events
+free(nate_boolinger).
+free(logan_lorr).
+free(sean_toolis).
+free(jeremy_suarez).
+free(drew_wilson).
 
-bffs(X,Y):- same_year(X,Y), same_stroke(X,Y).
+same_stroke(X, Y) :- (fly(X), fly(Y)); (back(X), back(Y)); (breast(X), breast(Y)); (free(X), free(Y)); (medley(X), medley(Y)); (distance(X), distance(Y)).
+
+same_year(X, Y) :- (freshman(X), freshman(Y)); (sophomore(X), sophomore(Y)); (junior(X), junior(Y)); (senior(X), senior(Y).
+
+best_friends(X, Y) :- same_year(X, Y), same_stroke(X, Y).
